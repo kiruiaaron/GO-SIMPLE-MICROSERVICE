da02e0044d3a37496ce5f224e9a236f65bd51828
kiruiaaron <kiruiaaron471@gmail.com>
2024-03-23 11:50:34 +0300

    new updates

--- api/main.go
+++ api/main.go
@@ -20,7 +20,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 func isAuthorized(endPoint func(http.ResponseWriter, *http.Request)) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r*http.Request){
 		if r.Header["Token"] != nil{
-			jwt.Parse(r.Header["Token"][0], func (token *jwt.Token)(interface{}, error)  {
+			token, err := jwt.Parse(r.Header["Token"][0], func (token *jwt.Token)(interface{}, error)  {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok{
 					return nil, fmt.Errorf(("Invalid signing method"))
 				}
@@ -40,8 +40,15 @@ func isAuthorized(endPoint func(http.ResponseWriter, *http.Request)) http.Handle
 				return MySigningKey, nil
 			})
 			if err != nil{
-				fmt.Printf(w, err.Error())
+				fmt.Fprintf(w, err.Error())
 			}
+
+			if token.Valid{
+				endPoint(w,r)
+			}
+			
+		}else{
+			fmt.Fprintf(w, "No authorization token provided")
 		}
 	})
 }
